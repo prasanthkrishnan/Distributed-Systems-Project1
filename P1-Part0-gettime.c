78a6ff35f62fddc7f4b5ef8a2a961038c44bf186
Prasanth Krishnan <knp281192@gmail.com>
2015-09-27 23:52:23 -0500

    Adds functionality to take minium elasped time

--- P1-Part0-gettime.c
+++ P1-Part0-gettime.c
@@ -7,7 +7,8 @@
 int main(int argc, char **argv)
 {
 	int n;
-	uint64_t diff;
+	uint64_t diff = 0;
+	uint64_t ans = 0;
 	struct timespec start, end;
 	
 	while(1)
@@ -15,15 +16,16 @@ int main(int argc, char **argv)
 		printf("%s\n", "ENter the number of loops:");
 		scanf("%d",&n);
 		int i=0;
-		clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
 		while(i<n){
+			clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+			clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 			++i;
+			diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+			ans = ans < diff ? ans : diff;
 		}
-		clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 
-		diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
 		printf("Number of loops = %d\n", n);
-		printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+		printf("minimum elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
 	}
 
 	return 0;
