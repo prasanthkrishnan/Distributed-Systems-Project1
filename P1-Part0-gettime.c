bcaf6f1bf76ff31fb1ee07c8a195d5e468d4ee44
Prasanth Krishnan <knp281192@gmail.com>
2015-09-28 02:09:46 -0500

    moves Initialization into the infinte loop

--- P1-Part0-gettime.c
+++ P1-Part0-gettime.c
@@ -7,17 +7,17 @@
 int main(int argc, char **argv)
 {
 	int n;
-	uint64_t diff = 0;
-	uint64_t min = BILLION;
-	uint64_t max = 0;
 	struct timespec start, end;
 
 	while(1)
 	{
-		printf("%s\n", "ENter the number of loops:");
+		uint64_t diff = 0;
+		uint64_t min = BILLION;
+		uint64_t max = 0;
+		printf("%s\n", "Enter the number of loops:");
 		scanf("%d",&n);
 		int i=0;
-		while(i<n){
+		while(i < n){
 			clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
 			clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 			++i;
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 
 		printf("Number of loops = %d\n", n);
 		printf("minimum elapsed time = %llu nanoseconds\n", min);
-		printf("minimum elapsed time = %llu nanoseconds\n", max);
+		printf("maximum elapsed time = %llu nanoseconds\n", max);
 	}
 
 	return 0;
