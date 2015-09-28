5ff274e191d8848168ce8471eeb6dbfdf5143e75
Prasanth Krishnan <knp281192@gmail.com>
2015-09-27 23:20:38 -0500

    Adds ability to measure elaspsed time of loops

--- P1-Part0-gettime.c
+++ P1-Part0-gettime.c
@@ -1,32 +1,30 @@
 #include <stdio.h>	
 #include <stdint.h>	
-#include <stdlib.h>	
 #include <time.h>
 
 #define BILLION 1000000000L
 
 int main(int argc, char **argv)
 {
+	int n;
 	uint64_t diff;
 	struct timespec start, end;
-	int i;
+	
+	while(true)
+	{
+		printf("%s\n", "ENter the number of loops:");
+		scanf("%d",&n);
+		int i=0;
+		clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+		while(i<n){
+			++i;
+		}
+		clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 
-	/* measure monotonic time */
-	clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
-	sleep(1);	/* do stuff */
-	clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
-
-	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
-
-	/* now re-do this and measure CPU time */
-	/* the time spent sleeping will not count (but there is a bit of overhead */
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
-	sleep(1);	/* do stuff */
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
-
-	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-	printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
+		diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+		printf("Number of loops = %d\n", n);
+		printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+	}
 
 	return 0;
 }
