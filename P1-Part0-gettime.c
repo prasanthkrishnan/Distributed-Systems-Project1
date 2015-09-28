7363a6c3735df488594eaf4aae5f3d1e9c74a3a4
Prasanth Krishnan <knp281192@gmail.com>
2015-09-28 02:03:26 -0500

    Adds min and max for diff over the loop

--- P1-Part0-gettime.c
+++ P1-Part0-gettime.c
@@ -8,13 +8,10 @@ int main(int argc, char **argv)
 {
 	int n;
 	uint64_t diff = 0;
-	uint64_t ans = BILLION;
+	uint64_t min = BILLION;
+	uint64_t max = 0;
 	struct timespec start, end;
-	
-		printf("minimum elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
-		printf("minimum elapsed time = %llu nanoseconds\n", diff);
-		printf("minimum elapsed time = %llu nanoseconds\n", (long long unsigned int) ans);
-		printf("minimum elapsed time = %llu nanoseconds\n",  ans);
+
 	while(1)
 	{
 		printf("%s\n", "ENter the number of loops:");
@@ -25,11 +22,13 @@ int main(int argc, char **argv)
 			clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 			++i;
 			diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-			ans = ans < diff ? ans : diff;
+			min = min < diff ? min : diff;
+			max = max > diff ? max : diff;
 		}
 
 		printf("Number of loops = %d\n", n);
-		printf("minimum elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+		printf("minimum elapsed time = %llu nanoseconds\n", min);
+		printf("minimum elapsed time = %llu nanoseconds\n", max);
 	}
 
 	return 0;
