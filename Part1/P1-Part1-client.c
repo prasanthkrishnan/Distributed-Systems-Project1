7dd1dbaa29a962a8d7423c304da9a8a443abcc00
Prasanth Krishnan Navaneetha Krishnan <prasanthkrishnan@macaroni-08.cs.wisc.edu>
2015-10-01 18:55:08 -0500

    Adds time counters

--- Part1/P1-Part1-client.c
+++ Part1/P1-Part1-client.c
@@ -4,6 +4,11 @@
 #include <stdint.h> 
 #include <time.h>
 #include <inttypes.h>
+#include <stdio.h>
+
+uint64_t getdiff(struct timespec start, struct timespec end) {
+	return (BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec);
+}
 
 void marshall_message(char msg[MSG_BUFFER_SIZE], int sequence_number) {
 	int i;
@@ -25,18 +30,27 @@ int main(int argc, char *argv[]) {
 	if(argc > 2)
 		sequence_number =  atoi(argv[2]);
 	printf("Number of packets: %d\n", number_of_packets);
+	// time holders
+	struct timespec Total_time_start,Total_time_end, RTT_start[number_of_packets], RTT_end[number_of_packets], overhead_start[number_of_packets], overhead_end[number_of_packets];
 
 	// //Port 0 - OS chooses port randomly
 	int sd = UDP_Open(0);
 	struct sockaddr_in addr, addr2;
 	assert(UDP_FillSockAddr(&addr, SERVER_HOSTNAME, SERVER_PORT) > -1);
 	int i;
+
+	clock_gettime(CLOCK_MONOTONIC, &Total_time_start);
 	for (i = sequence_number; i < sequence_number + number_of_packets; ++i)
 	{
 		// i is used as sequence number
+		// Overhead start
+		clock_gettime(CLOCK_MONOTONIC, &(RTT_start[i - sequence_number]));
+		clock_gettime(CLOCK_MONOTONIC, &(overhead_start[i - sequence_number]));
 		char packet[MSG_BUFFER_SIZE];
 		int wc;
 		marshall_message(packet,i);
+		// Overhead end
+		clock_gettime(CLOCK_MONOTONIC, &(overhead_end[i - sequence_number]));
 		timeout:
 		printf("%s %d\n", "Sending packet sequence_number:", i);
 		wc = UDP_Write(sd, &addr, packet, MSG_BUFFER_SIZE);
@@ -56,6 +70,20 @@ int main(int argc, char *argv[]) {
 			printf("%s %d\n", "Write failed for sequence_number:", i);
 			goto timeout;
 		}
+		clock_gettime(CLOCK_MONOTONIC, &(RTT_end[i - sequence_number]));
+	}
+	clock_gettime(CLOCK_MONOTONIC, &Total_time_end);
+
+	printf("Total time elapsed = %"PRIu64" nanoseconds\n", getdiff(Total_time_start,Total_time_end));
+
+	for (i = 0; i < number_of_packets; ++i)
+	{
+		printf("%"PRIu64" ", getdiff(RTT_start[i],RTT_end[i]));
+	}
+	printf("\n");
+	for (i = 0; i < number_of_packets; ++i)
+	{
+		printf("%"PRIu64" ", getdiff(overhead_start[i],overhead_end[i]));
 	}
 	return 0;
 }
--- Part1/constants.h
+++ Part1/constants.h
@@ -6,4 +6,5 @@
 #define TIMEOUT_IN_SECONDS 3
 #define SEQUENCE_NUMBER 1
 #define SERVER_HOSTNAME "localhost"
+#define BILLION 1000000000L
 #endif
